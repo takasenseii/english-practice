2b1db5a2993aa7eceba86e96f13b2ae5ee6b6493
takasenseii <alexander.takakusagi@gmail.com>
2026-09-12 23:30:12 +0200

    Add files via upload

--- docs/js/tutor-materials.js
+++ docs/js/tutor-materials.js
@@ -2,5 +2,6 @@ import rosaParks from "./tutor-materials/rosa-parks.js";
 import entrepreneur from "./tutor-materials/entrepreneur.js";
 import sixTipsDivided from "./tutor-materials/six-tips-divided.js";
 import startupSuccess from "./tutor-materials/startup-success.js";
+import benefitsOfFailure from "./tutor-materials/benefits-of-failure.js";
 
-export const tutorMaterials = [rosaParks, entrepreneur, sixTipsDivided, startupSuccess];
+export const tutorMaterials = [rosaParks, entrepreneur, sixTipsDivided, startupSuccess, benefitsOfFailure];
